7ac9dfdbd9d387958064eb9d17c18974baaf8fe9
Pablo Felipe <pfsa1810@hotmail.com>
2025-07-08 19:15:20 -0300

    Atualizações no sistema offline da guarita

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/DIMIVIGLOGO.png'
+  '/DIMIVIGLOGO.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
 ];
 
 self.addEventListener('install', (event) => {
@@ -14,6 +15,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
