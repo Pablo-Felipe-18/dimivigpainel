d4a25d88e13c8de048d692374804791daca93ca5
Pablo Felipe <pfsa1810@hotmail.com>
2025-07-08 21:24:40 -0300

    Atualização: fusão completa com suporte a modo offline e sincronização automática

--- service-worker.js
+++ service-worker.js
@@ -7,31 +7,16 @@ const urlsToCache = [
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
 ];
 
-// Instalação
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Ativação - Limpa caches antigos
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Intercepta requisições
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match('/index.html');
-      });
+      return response || fetch(event.request);
     })
   );
 });
